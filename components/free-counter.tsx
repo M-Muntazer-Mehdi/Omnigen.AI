6b4cc1b9f9337fd5eb10950acecd78a5d0892751
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-07-26 14:07:18 +0500

    feat(free-counter): improve logic and UI

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -32,7 +32,7 @@ export const FreeCounter: FC<FreeCounterProps> = ({ apiLimitCount = 0, isPro = f
 
   return (
     <div className="px-3">
-      <Card className="bg-white/10 border-0">
+      <Card className="bg-white/20 border-0">
         <CardContent className="py-6">
           <div className="text-center text-sm text-white mb-4 space-y-2">
             <p>
