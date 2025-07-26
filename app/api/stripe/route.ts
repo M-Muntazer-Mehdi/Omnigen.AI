2d7fd3a378a405d8b196c58251afe297a7b38f58
Mehdi Rizvi <muntazer.mehdi.rizvi@gmail.com>
2025-07-26 18:51:15 +0500

    Build 1.4

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,9 +1,11 @@
+export const runtime = "nodejs";
+
 import { auth, currentUser } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-
 import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
 import { absoluteUrl } from "@/lib/utils";
+
 
 const settingsUrl = absoluteUrl("/settings");
 
